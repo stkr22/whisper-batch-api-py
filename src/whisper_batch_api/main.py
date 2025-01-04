d7ef69894cebb20fb08012992f59a230700ce895
stkr22 <stkr22@github.com>
2025-01-04 17:19:45 +0000

    updating to batched inference

--- src/whisper_batch_api/main.py
+++ src/whisper_batch_api/main.py
@@ -59,6 +59,9 @@ async def transcribe(
     audio_bytes = await file.read()
     audio_np = np.frombuffer(audio_bytes, dtype=np.float32)
     if "distil" in os.getenv("WHISPER_MODEL", "distil-medium.en"):
+        batched_model = faster_whisper.BatchedInferencePipeline(
+            model=transcriber_engine
+        )
         optimal_chunks = (
             15 * 16000
         )  # filling to recommended size of 15s https://github.com/huggingface/distil-whisper/blob/a07edc4f3840bca33c5dc495303196bf6d0b2c40/README.md?plain=1#L131
@@ -66,15 +69,25 @@ async def transcribe(
         pad_size = int(desired_length) - audio_np.size
         padded_array = np.pad(audio_np, (0, pad_size), "constant", constant_values=(0))
         audio_np = padded_array
-        segments, info = transcriber_engine.transcribe(
+        segments, info = batched_model.transcribe(
             audio_np,
             language="en",
+            temperature=0,
+            batch_size=2,
             max_new_tokens=128,
             condition_on_previous_text=False,
         )
     else:
-        segments, info = transcriber_engine.transcribe(audio_np)
+        segments, info = transcriber_engine.transcribe(
+            audio_np,
+            max_new_tokens=224,
+            beam_size=5,
+            temperature=0,
+            language="en",
+            condition_on_previous_text=False,
+        )
     final_text = ""
+
     for segment in segments:
         final_text += segment.text
 
